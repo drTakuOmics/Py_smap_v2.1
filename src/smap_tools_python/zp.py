e270551e8f38806cc2ff7d822c58d6da321548d9
drTakuOmics <121636288+drTakuOmics@users.noreply.github.com>
2025-09-06 15:41:47 -0400

    zp.py

--- src/smap_tools_python/zp.py
+++ src/smap_tools_python/zp.py
@@ -1,4 +1,4 @@
-+def zp(num_in, N, pad_char="0"):
-+    """Zero-pad a number or string to a given width."""
-+    s = str(num_in)
-+    return s.rjust(N, pad_char)
+def zp(num_in, N, pad_char="0"):
+    """Zero-pad a number or string to a given width."""
+    s = str(num_in)
+    return s.rjust(N, pad_char)
