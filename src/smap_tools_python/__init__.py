b0461285bc702affb65f29402e698ee598b10704
drTakuOmics <121636288+drTakuOmics@users.noreply.github.com>
2025-09-06 15:38:52 -0400

    __init__.py

--- src/smap_tools_python/__init__.py
+++ src/smap_tools_python/__init__.py
@@ -1,16 +1,16 @@
-+"""Python utilities for SMAP tools."""
-+
-+from .cos_mask import variable_cos_mask, rrj
-+from .quaternion import Quaternion
-+from .constants import def_consts
-+from .zp import zp
-+from .fov import fov_to_num
-+
-+__all__ = [
-+    "variable_cos_mask",
-+    "rrj",
-+    "Quaternion",
-+    "def_consts",
-+    "zp",
-+    "fov_to_num",
-+]
+"""Python utilities for SMAP tools."""
+
+from .cos_mask import variable_cos_mask, rrj
+from .quaternion import Quaternion
+from .constants import def_consts
+from .zp import zp
+from .fov import fov_to_num
+
+__all__ = [
+    "variable_cos_mask",
+    "rrj",
+    "Quaternion",
+    "def_consts",
+    "zp",
+    "fov_to_num",
+]
