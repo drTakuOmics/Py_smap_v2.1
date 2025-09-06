6b880c0a9f736733d9c7a69bf9b5ad7bb9053867
drTakuOmics <121636288+drTakuOmics@users.noreply.github.com>
2025-09-06 15:30:21 -0400

    __init__.py

--- src/smap_tools_python/__init__.py
+++ src/smap_tools_python/__init__.py
@@ -1,8 +1,3 @@
-diff --git a//dev/null b/src/smap_tools_python/__init__.py
-index 0000000000000000000000000000000000000000..00a6cb65c23c03804a8a4edd48c3c7f781d2d87b 100644
---- a//dev/null
-+++ b/src/smap_tools_python/__init__.py
-@@ -0,0 +1,16 @@
 +"""Python utilities for SMAP tools."""
 +
 +from .cos_mask import variable_cos_mask, rrj
