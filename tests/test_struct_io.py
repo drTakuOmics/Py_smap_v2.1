0d9b40354d30556d899c403f7050f4318f3d2207
drTakuOmics <121636288+drTakuOmics@users.noreply.github.com>
2025-09-07 19:26:56 -0400

    Use Bio.PDB for structure parsing

--- tests/test_struct_io.py
+++ tests/test_struct_io.py
@@ -28,3 +28,30 @@ def test_read_pdb_file_basic():
     assert np.allclose(xyz[:, 0], [29.342, -33.809, -75.427])
     assert np.isclose(data["bFactor"][0], 142.22)
     assert np.isclose(data["occ"][0], 1.00)
+
+
+def test_read_pdb_file_mmcif():
+    cif_path = MODEL_DIR / "6ek0_LSU.cif"
+    data = read_pdb_file(str(cif_path))
+    xyz = data["xyz"]
+    assert xyz.shape[0] == 3
+    assert data["atomList"][0] == "C5'"
+    assert data["chainIDs"][0] == "L5"
+    assert np.allclose(xyz[:, 0], [-20.637, 30.989, 98.976])
+    assert np.isclose(data["bFactor"][0], 109.77)
+    assert np.isclose(data["occ"][0], 1.0)
+
+
+def test_read_pdb_file_altloc(tmp_path):
+    pdb_content = (
+        "ATOM      1  CA AASN A   1      11.104  13.207  10.718  0.50 20.00           C\n"
+        "ATOM      1  CA BASN A   1      11.204  13.307  10.818  0.50 25.00           C\n"
+        "TER\n"
+        "END\n"
+    )
+    pdb_path = tmp_path / "altloc.pdb"
+    pdb_path.write_text(pdb_content)
+    data = read_pdb_file(str(pdb_path))
+    assert np.allclose(data["xyz"][:, 0], [11.104, 13.207, 10.718])
+    assert np.isclose(data["occ"][0], 0.5)
+    assert data["altLoc"][0] == "A"
