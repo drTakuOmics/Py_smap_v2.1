2c83b2bd96295949e84f72674608106a77f6ab8f
drTakuOmics <121636288+drTakuOmics@users.noreply.github.com>
2025-09-08 13:21:59 -0400

    Return weights from backproject

--- tests/test_backproject.py
+++ tests/test_backproject.py
@@ -6,15 +6,13 @@ def test_backproject_identity_slice():
     patch = np.array([[1, 2], [3, 4]], float)
     patches = patch[:, :, None]
     R = np.eye(3)[None, ...]
-    vol = backproject(patches, R, pad_size=4)
-    expected = np.array(
-        [
-            [2.5, 2.5, 2.5, 2.5],
-            [2.5, 1.0, 2.0, 2.5],
-            [2.5, 3.0, 4.0, 2.5],
-            [2.5, 2.5, 2.5, 2.5],
-        ]
-    )
-    assert np.allclose(vol[:, :, 2], expected)
-    assert np.allclose(vol[:, :, :2], 0)
-    assert np.allclose(vol[:, :, 3], 0)
+    vol, weights, other = backproject(patches, R, pad_size=4)
+
+    reconstructed = np.divide(vol, weights, out=np.zeros_like(vol), where=weights > 0)
+    expected = np.zeros((4, 4))
+    expected[1:3, 1:3] = patch
+
+    assert np.allclose(reconstructed[:, :, 2], expected)
+    assert np.allclose(reconstructed[:, :, :2], 0)
+    assert np.allclose(reconstructed[:, :, 3], 0)
+    assert np.allclose(other, 0)
